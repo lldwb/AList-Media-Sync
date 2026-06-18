0f3db93d947fe9753a77aebf19191798f6bea35e
安然的尾巴 <3247187440@qq.com>
2026-06-19 00:51:53 +0800

    项目初始化

---
+++ src/test/java/top/lldwb/alistmediasync/AListMediaSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lldwb.alistmediasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AListMediaSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
